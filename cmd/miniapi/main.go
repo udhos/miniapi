446063f494271daacb0c5d62c488e81e19f8dd5a
udhos <udhos>
2023-02-03 16:54:38 -0300

    Reply with server version.

--- cmd/miniapi/main.go
+++ cmd/miniapi/main.go
@@ -94,8 +94,8 @@ func response(w http.ResponseWriter, r *http.Request, status int, message string
 	if errHost != nil {
 		log.Printf("hostname error: %v", errHost)
 	}
-	reply := fmt.Sprintf(`{"message":"%s","status":"%d","path":"%s","method":"%s","host":"%s","serverHostname":"%s"}`,
-		message, status, r.RequestURI, r.Method, r.Host, hostname)
+	reply := fmt.Sprintf(`{"message":"%s","status":"%d","path":"%s","method":"%s","host":"%s","serverHostname":"%s","serverVersion":"%s"}`,
+		message, status, r.RequestURI, r.Method, r.Host, hostname, version)
 	httpJSON(w, reply, status)
 }
 
