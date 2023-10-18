90df02e7435398110f5aa9ec4226c4b3ff29a105
udhos <udhos>
2023-10-18 18:21:01 -0300

    Rebuild image with security fixes.

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-version=$(go run ./cmd/miniapi -version | awk '{ print $2 }' | awk -F= '{ print $2 }')
+app=miniapi
+
+version=$(go run ./cmd/$app -version | awk '{ print $2 }' | awk -F= '{ print $2 }')
 
 echo version=$version
 
-docker build --no-cache \
-    -t udhos/miniapi:latest \
-    -t udhos/miniapi:$version \
+docker build \
+    --no-cache \
+    -t udhos/$app:latest \
+    -t udhos/$app:$version \
     -f docker/Dockerfile .
+
+echo "push: docker push udhos/$app:$version; docker push udhos/$app:latest"
